fad3ba52145ddec1e37924f11bfc7548e7bd7e23
xionghaibo <1489280205@qq.com>
2017-01-16 17:32:17 +0800

    004TextInput组件

--- README.md
+++ README.md
@@ -22,3 +22,8 @@
 ![image](https://github.com/haibofaith/React-native/blob/master/image/003Image.png)
 
 
+<br/>
+<a href="http://www.lcode.org/%E3%80%90react-native%E5%BC%80%E5%8F%91%E3%80%91react-native%E6%8E%A7%E4%BB%B6%E4%B9%8Btextinput%E7%BB%84%E4%BB%B6%E8%AE%B2%E8%A7%A3%E4%B8%8Eqq%E7%99%BB%E5%BD%95%E7%95%8C%E9%9D%A2%E5%AE%9E%E7%8E%B011/"/>004TextInput组件</a>
+<br/>
+![image](https://github.com/haibofaith/React-native/blob/master/image/004TextInput.png)
+
--- index.android.js
+++ index.android.js
@@ -3,29 +3,34 @@ import {
   AppRegistry,
   Text,
   View,
-  Image,
+  TextInput,
   StyleSheet,
 } from 'react-native';
 
 var HelloWor = React.createClass({
   render: function() {
     return (
-          /**
-          *1. 注意本地路径地址，根目录为HelloWorld项目文件夹(此注释不能放在view内部)
-          */
-      <View style = {{marginLeft:10,marginRight:10}}>
-          <Text style = {styles.titleBase}>测试本地图片</Text>
-          <Image source = {require('./android/img/my_icon.jpg')}  style = {{width:40,height:40}}/>
-          <Text style = {styles.titleBase}>测试app资源文件</Text>
-          <Image source = {{uri:'ic_launcher'}} style = {{width:40,height:40}}/>
-          <Text style = {styles.titleBase}>测试加载网络图片</Text>
-          <Image source = {{uri:'https://ss0.baidu.com/-Po3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=8e07ecabb7fb4316051f7d7a10a54642/5882b2b7d0a20cf482c772bf73094b36acaf997f.jpg'}} style = {{width:100,height:100}}/>
+      /**
+      TextInput组件设置高度40,边框粗细和边框的颜色，切可以输入多行信息。
+      第二个TextInput设置左右额边距以及自动获取到焦点。
+      第三个TextInput设置不可编辑状态。
+      */
+      <View style = {styles.container}>
+          <Text style = {styles.titleBase}>Welcome To React Native</Text>
+          <TextInput style = {{height:40,borderColor:'red',borderWidth:1}} multiline={true} defaultValue='默认信息1'/>
+          <TextInput style = {{marginLeft:10,marginRight:10}} autoFocus={true} defaultValue='默认信息2'/>
+          <TextInput editable={false} defaultValue='默认信息3'/>
       </View>
     );
   }
 });
 
 var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection:'column',
+    backgroundColor: '#F5FCFF',
+  },
   titleBase :{
     margin:10,
     textAlign:'center',
